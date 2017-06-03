e49e26c9c5490ff08d5e81c5dabe90c49d0878ad
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2017-06-03 11:19:15 +0600

    model & migration add

---
+++ database/migrations/2017_06_03_050913_create_contests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->string('url', 255)->unique();
+            $table->dateTime('start');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contests');
+    }
+}
